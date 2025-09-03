1b8942171a64dad3e26c17264ba250ab9ea0732e
zman43g <zman3@yandex.ru>
2025-09-03 16:56:08 +0700

    ДЗ №5 (внесены исправления)

--- src/Main.java
+++ src/Main.java
@@ -6,42 +6,47 @@ public class Main {
         byte clientOs = 1;
         switch (clientOs) {
             case 0:
-                System.out.println("Установите версию приложения для Ios.");
+                System.out.println("Установите версию приложения для iOS по ссылке");
                 break;
             case 1:
-                System.out.println("Установите версию приложения для Android.");
+                System.out.println("Установите версию приложения для Android по ссылке");
                 break;
         }
         System.out.println("Задача 2");
         short clientDeviceYear = 2015;
-        if (clientDeviceYear >= 2015) {
+        clientOs = 1;
+        boolean newDevice = clientDeviceYear >= 2015;
+                if (newDevice) {
             switch (clientOs) {
                 case 0:
-                    System.out.println("Установите версию приложения для Ios.");
+                    System.out.println("Установите версию приложения для iOS по ссылке");
                     break;
                 case 1:
-                    System.out.println("Установите версию приложения для Android.");
+                    System.out.println("Установите версию приложения для Android по ссылке");
                     break;
             }
-        } else {
+        }  else {
             switch (clientOs) {
                 case 0:
-                    System.out.println("Установите облегченную версию приложения для Ios по ссылке.");
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
                     break;
                 case 1:
                     System.out.println("Установите облегченную версию приложения для Android по ссылке.");
                     break;
             }
+
         }
         System.out.println("Задача 3");
-        short year = 2021;
+        short year = 1700;
         boolean leapYear = (year - 1580) % 4 == 0 && year % 100 != 0;
-        if (year % 400 == 0) {
-            System.out.println(year + " год является високосным.");
-        } else if (leapYear) {
-            System.out.println(year + " год является високосным.");
-        } else {
-            System.out.println(year + " год не является високосным.");
+        if (year > 1584) {
+            if (year % 400 == 0) {
+                System.out.println(year + " год является високосным.");
+            } else if (leapYear) {
+                System.out.println(year + " год является високосным.");
+            } else {
+                System.out.println(year + " год не является високосным.");
+            }
         }
 
         System.out.println("Задача 4");
@@ -64,29 +69,30 @@ public class Main {
 
         System.out.println("Задача 5");
         byte monthNumber = 12;
-        if (monthNumber <= 12) {
-            switch (monthNumber) {
-                case 12:
-                case 1:
-                case 2:
-                    System.out.println("Это зимний месяц");
-                    break;
-                case 3:
-                case 4:
-                case 5:
-                    System.out.println("Это весенний месяц");
-                    break;
-                case 6:
-                case 7:
-                case 8:
-                    System.out.println("Это весенний месяц");
-                    break;
-                case 9:
-                case 10:
-                case 11:
-                    System.out.println("Это весенний месяц");
-                    break;
-            }
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Это зимний месяц");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Это весенний месяц");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Это весенний месяц");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Это весенний месяц");
+                break;
+            default:
+                System.out.println("Такого месяца нет");
+                break;
         }
     }
 
